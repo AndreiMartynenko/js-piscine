de986ba5fd3604b369ded17aba8bba984344d70c
AndrewLondon <andrewmartofficial@gmail.com>
2023-03-10 00:31:30 +0000

    repeat

--- repeat.js
+++ repeat.js
@@ -15,3 +15,15 @@ function repeat(string, number) {
     return stringToRepeat
 }
 console.log(repeat("Hello", 5))
+
+
+/*
+In this implementation, we initialize an empty string 
+repeatedString to store the result of repeating string. 
+We also initialize a counter variable to keep track of 
+how many times we've repeated string.
+We then enter a while loop that runs num number of times. 
+On each iteration, we append string to repeatedString 
+using the += operator and decrement counter by 1.
+Finally, we return the fully repeated string.
+*/
